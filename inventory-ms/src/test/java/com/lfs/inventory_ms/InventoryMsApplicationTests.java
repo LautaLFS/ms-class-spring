04996bb180b13eaa2b4c3f509de567c8f0c38cd7
Lautaro <lautafuriasse16@gmail.com>
2024-07-07 16:13:17 -0300

    Initial commit

---
+++ inventory-ms/src/test/java/com/lfs/inventory_ms/InventoryMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lfs.inventory_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ product-ms/src/test/java/com/lfs/product_ms/ProductMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lfs.product_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
